43041a2c9a231421a474ca8bf9a8a87f070f1b05
taksenov <taksenov@gmail.com>
2014-11-09 21:06:06 +0500

    JS and HTML-Code cleaning

--- js/main.js
+++ js/main.js
@@ -5,52 +5,7 @@ $(document).ready(function ($) {
     $('.btn-navbar').click( function() {
 	    $('html').toggleClass('expanded');
     });
-    
-    
-    // Slide Toggles
-    
-    $('#section3 .button').on('click', function () {
-	    
-	    var section = $(this).parent();
-		
-		section.toggle();
-	    section.siblings(".slide").slideToggle('2000', "easeInQuart");
-	});
-	
-	$('#section3 .read-more').on('click', function () {
-	    
-	    var section = $(this).parent();
-		
-		section.toggle();
-	    section.siblings(".slide").slideToggle('2000', "easeInQuart");
-	});
-	
-	$('#section4 .article-tags li').on('click', function () {
-	    
-	    var section = $(this).parents('.span4');
-	    var category = $(this).attr('data-blog');
-	    var articles = section.siblings();
-	    
-	    // Change Tab BG's
-	    $(this).siblings('.current').removeClass('current');
-	    $(this).addClass('current');
-		
-		// Hide/Show other articles
-	    section.siblings('.current').removeClass('current').hide();
-	    
-    	$(articles).each(function (index) {
-	    	
-	    	var newCategory = $(this).attr('data-blog');
-	    	
-	    	if ( newCategory == category ) {
-		    	$(this).slideDown('1000', "easeInQuart").addClass('current');
-	    	}
-	    });
 
-	});
-	
-	
-		
 	// Waypoints Scrolling
 	
 	var links = $('.navigation').find('li');
@@ -109,35 +64,7 @@ $(document).ready(function ($) {
     });
   
     // Snap to scroll (optional)
-    
-    /*
 
-    section.waypoint(function (direction) {
-
-        var nextpos = $(this).attr('data-section');
-        var prevpos = $(this).prev().attr('data-section');
-
-        if (nextpos != 1) {
-	        if (direction === 'down') {
-	            htmlbody.animate({
-		            scrollTop: $('.section[data-section="' + nextpos + '"]').offset().top
-		        }, 750, 'easeOutQuad');
-	        }
-	        else {
-	            htmlbody.animate({
-		            scrollTop: $('.section[data-section="' + prevpos + '"]').offset().top
-		        }, 750, 'easeOutQuad');
-	        }
-        }
-        
-
-    }, { offset: '60%' });	
-    
-    */
-   
-       
-    
-    
     // Redirect external links
 	
 	$("a[rel='external']").click(function(){
